9f9350d650dea37f76e5aba2ec1fe909643e40b2
shota-ishi73 <1473shota@gmail.com>
2021-03-15 21:37:43 +0900

    修正１

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
-  before_action :authenticate_user!, expect: :index
+  before_action :authenticate_user!, only: [:create, :destroy, :update]
 
   private
   def configure_permitted_parameters
